31732146ceda425014894894a538d4e1dd7112cd
BargainBois <35859689+BargainBois@users.noreply.github.com>
2018-01-26 20:34:32 -0800

    Add files via upload

---
+++ BADUSB_CODE.ino
@@ -0,0 +1,50 @@
+#include <Keyboard.h>
+void setup() {
+  pinMode(5, INPUT_PULLUP);
+  #PLACE CODE HERE TO BE RUN BEFORE PRESSING THE BUTTON
+}
+void loop(){
+  while (digitalRead(5) == HIGH) {
+  }
+  #PLACE CODE HERE TO BE USED AFTER PRESSING BUTTON
+}
+
+
+
+
+/*
+############## CUSTOM COMMANDS ###############
+#TypeString:Types A String                   #
+#EnterString:Types String And Terminates Line#
+#PressKey:Presses A Key On The Keyboard      #
+#ChangeLED:Changes LED State 1 = ON | 0 = OFF#
+##############################################
+*/
+
+
+
+
+
+
+
+
+
+
+void TypeString(String MyString)
+{
+    Keyboard.print(MyString);
+}
+void EnterString(String MyVar1)
+{
+    Keyboard.println(MyVar1);
+}
+
+void ChangeLED(int hrl){
+  digitalWrite(LED_BUILTIN, hrl);
+};
+
+void PressKey(char KEYNAME){
+  Keyboard.press(KEYNAME);
+  delay(250);
+  Keyboard.release(KEYNAME);
+};
